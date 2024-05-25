0a40492460e87de2c55ae90a8399b55e95cdfe30
Leslie-L <leslie.centeno@galileo.edu>
2024-05-24 20:34:30 -0600

    basic routes

--- backend/src/routes/tasks.routes.js
+++ backend/src/routes/tasks.routes.js
@@ -7,8 +7,22 @@ router.get('/',(req,res)=>{
     res.status(200).send("hi tasks... this es")
 })
 router.get('/:id',(req,res)=>{
-    const {id} = req.query;
+    const {id} = req.params;
     res.status(200).send(id )
+})
+router.post('/',(req,res)=>{
+    const data= req.body
+    res.status(200).send("create a new task")
+})
+router.patch('/:id',(req,res)=>{
+    const {id} = req.params;
+    const data= req.body
+
+    res.status(200).send("update a task")
+})
+router.delete('/:id',(req,res)=>{
+    const {id} = req.params;
+    res.status(200).send("delete a task")
 })
 
 module.exports = router;
