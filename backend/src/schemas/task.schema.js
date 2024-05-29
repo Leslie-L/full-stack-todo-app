a6ff9da1749565884704ca35b3af7bd55b646d1f
Leslie-L <leslie.centeno@galileo.edu>
2024-05-28 19:46:57 -0600

    changing schemas

--- backend/src/schemas/task.schema.js
+++ backend/src/schemas/task.schema.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 
-const id = Joi.number()
+const id = Joi.number();
 const task = Joi.string().min(1);
 const finished = Joi.boolean();
 const date = Joi.date();
@@ -8,11 +8,12 @@ const date = Joi.date();
 const createTask = Joi.object({
     task: task.required(),
     date: date.required(),
+    id:id.required(),
+    finished:finished.required()
 })
 const updateTask = Joi.object({
     id:id.required(),
     task: task,
-    date: date,
     finished:finished  
 })
 const getTask = Joi.object({
