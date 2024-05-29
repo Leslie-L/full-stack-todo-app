5dc5045454f5d9fa4511a75632e13e8ae1845eca
Leslie-L <leslie.centeno@galileo.edu>
2024-05-28 19:47:03 -0600

    new methods

--- backend/src/model/tasks.model.js
+++ backend/src/model/tasks.model.js
@@ -11,9 +11,15 @@ class ModelTasks {
         const task = await db.query('SELECT * FROM tasks WHERE id=$1',[id])
         return task.rows[0]
     }
-    async insertTask({user,task,date}){
-        const insert = await db.query('INSERT INTO tasks(user, task,finished,date) VALUES ($1,$2,$3,$4) RETURNING *',[user,task,false,date ])
+    async insertTask({id,finished,task,date}){
+        const insert = await db.query('INSERT INTO tasks("user", taks,finished,date) VALUES ($1,$2,$3,$4) RETURNING *',[id,task,finished,date ])
         return insert.rows[0]
+    }
+    async updateTask(id,data){
+        await db.query('UPDATE tasks SET task = $1, finished = $2 WHERE id = $3',[data.task,data.finished,id])
+    }
+    async deleteTaks(id){
+        await db.query('DELETE FROM tasks WHERE id=$1;',[id])
     }
 }
 module.exports = ModelTasks
